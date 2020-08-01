230a363412ce2f8131b7ea7dbcc64b050f5a717e
mamadou alpha barry <mamadou.barry1105@gmail.com>
2020-08-01 23:51:27 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,16 @@
+ turns = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      turns += 1
+    end
+  end
+  return turns
+end
+
+def current_player(board)
+  turns = turn_count(board)
+  if turns % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
